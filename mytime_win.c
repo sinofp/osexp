e3c9ae27f61a32091618cfba32d1b0615eba9442
unknown <achinese@live.cn>
2019-11-18 18:00:37 +0800

    add mytime ver.windows

---
+++ mytime_win.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main(int argc, char * argv[]){
+    // use `chcp 65001` first
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    memset(&si, 0, sizeof si);
+    memset(&pi, 0, sizeof pi);
+    si.cb = sizeof si;
+
+    if (argc < 2) {
+        puts("not enough argument");
+    }
+
+    for (int i = 2; i < argc; i++) {
+        strcat(argv[1], " ");
+        strcat(argv[1], argv[2]);
+    }
+
+    if(!CreateProcess( NULL, argv[1], NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+        puts("create process failed");
+        return 0;
+    }
+    SYSTEMTIME start, end;
+    GetSystemTime(&start);
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    GetSystemTime(&end);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    if (end.wMilliseconds < start.wMilliseconds) {
+        end.wSecond -= 1;
+        end.wMilliseconds += 1000;
+    }
+    if (end.wSecond < start.wSecond) {
+        end.wMinute -= 1;
+        end.wSecond += 60;
+    }
+    if (end.wMinute < start.wMinute) {
+        end.wHour -= 1;
+        end.wMinute += 60;
+    }
+
+    printf("%d小时%d分钟%d秒%d毫秒\n", end.wHour-start.wHour, end.wMinute-start.wMinute, end.wSecond-start.wSecond, end.wMilliseconds-start.wMilliseconds);
+    return 0;
+}
