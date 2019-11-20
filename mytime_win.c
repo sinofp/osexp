c0e832fcb7164ea035bf87b737c2cf008c48e387
unknown <achinese@live.cn>
2019-11-20 19:21:46 +0800

    add system("chcp 65001")
    forgot to push!!!!!

--- mytime_win.c
+++ mytime_win.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <windows.h>
 
-int main(int argc, char * argv[]){
-    // use `chcp 65001` first
+int main(int argc, char* argv[]) {
+    system("chcp 65001");  // to use utf-8 in shell
+
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
     memset(&si, 0, sizeof si);
@@ -19,7 +20,8 @@ int main(int argc, char * argv[]){
         strcat(argv[1], argv[i]);
     }
 
-    if(!CreateProcess(NULL, argv[1], NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+    if (!CreateProcess(NULL, argv[1], NULL, NULL, FALSE, 0, NULL, NULL, &si,
+                       &pi)) {
         puts("create process failed");
         return 0;
     }
@@ -43,6 +45,8 @@ int main(int argc, char * argv[]){
         end.wMinute += 60;
     }
 
-    printf("%d小时%d分钟%d秒%d毫秒\n", end.wHour-start.wHour, end.wMinute-start.wMinute, end.wSecond-start.wSecond, end.wMilliseconds-start.wMilliseconds);
+    printf("%d小时%d分钟%d秒%d毫秒\n", end.wHour - start.wHour,
+           end.wMinute - start.wMinute, end.wSecond - start.wSecond,
+           end.wMilliseconds - start.wMilliseconds);
     return 0;
 }
