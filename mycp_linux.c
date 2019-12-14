b75f09d07c231e81b3f13e67c7a45499b3e314f9
sinofp <achinese@live.cn>
2019-12-14 20:09:39 +0800

    Finish mycp linux ver
    让nftw访问完一个文件夹里所有文件再访问文件夹，
    这样好设置文件夹的最后访问时间，否则设完了再复制文件会更改文件夹的时间
    
    单独处理符号链接（FTW_PHYS + realpath + symlink + AT_SYMLINK_NOFOLLOW）
    
    另外看好多系统调用分绝对路径和相对路径的版本，
    怕出错直接在main函数里用realpath提取绝对路径当参数了。

--- mycp_linux.c
+++ mycp_linux.c
@@ -1,14 +1,15 @@
-#define _XOPEN_SOURCE 700 // for nftw & utimensat
+#define _XOPEN_SOURCE 700  // for nftw & utimensat
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <ftw.h>
 #include <linux/limits.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/sendfile.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <errno.h>
 
 #define handle_error(msg)   \
     do {                    \
@@ -16,72 +17,90 @@
         exit(EXIT_FAILURE); \
     } while (0)
 
-char to[PATH_MAX], from[PATH_MAX], next_to[PATH_MAX];
+char to[PATH_MAX], from[PATH_MAX], next_to[PATH_MAX], rpath[PATH_MAX];
 char content_buf[1024];
 
 size_t start_index;
 
-int walk_action(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
-    // 文件名 fpath + ftwbuf->base
-    // 文件路径 fpath
-    // 文件大小 (intmax_t) sb->st_size
-    sprintf(next_to, "%s%s", to, strlen(fpath) < start_index ? "" : fpath + start_index);
-    printf("current: %s\n", fpath);
+int mkdir_p(char path[]) {
+    // 存在就直接返回执行mkdir存在的返回值
+    struct stat sb;
+    if (0 == stat(path, &sb)) {
+        errno = EEXIST;
+        return -1;
+    }
+
+    char* p = NULL;
+    sprintf(rpath, "%s", path);  // 反正还不到处理符号链接的时候，借用了
+    if ('/' == rpath[strlen(rpath) - 1]) rpath[strlen(rpath) - 1] = 0;
+    for (p = rpath + 1; *p; p++) {
+        if ('/' == *p) {
+            *p = 0;
+            if (-1 == mkdir(rpath, S_IRWXU) && EEXIST != errno) return -1;
+            *p = '/';
+        }
+    }
+    return mkdir(rpath, S_IRWXU);
+}
+
+int walk_action(const char* fpath, const struct stat* sb, int typeflag,
+                struct FTW* ftwbuf) {
+    sprintf(next_to, "%s%s", to,
+            strlen(fpath) < start_index ? "" : fpath + start_index);
     struct timespec times[2];
     times[0] = sb->st_atim;
     times[1] = sb->st_mtim;
-    if (typeflag == FTW_D) {
-        printf("%s is a folder\n", fpath);
-        if (-1 == mkdir(next_to, sb->st_mode)) {
-            if (EEXIST == errno) printf("%s exists\n", fpath + ftwbuf->base);
-            else handle_error("mkdir failed");
+    if (typeflag == FTW_DP) {
+        // 可能是个空文件夹，为了保险还得mkdir
+        if (-1 == mkdir_p(next_to)) {
+            if (EEXIST != errno) handle_error("mkdir failed");
         }
-        if (-1 == utimensat(AT_FDCWD, next_to, times, 0)) handle_error("set direcory time");
-    } else if (typeflag == FTW_F) {
-        printf("copy %s to %s\n", fpath, next_to);
-        int from_fd = open(fpath, O_RDONLY);
-        if (-1 == from_fd) handle_error("open source file");
-
+        if (-1 == chmod(next_to, sb->st_mode))
+            handle_error("change directory mod");
+        if (-1 == utimensat(AT_FDCWD, next_to, times, 0))
+            handle_error("set directory time");
+    } else if (FTW_F == typeflag || FTW_SL == typeflag) {
         // 创建文件夹
-//        size_t null_index = strlen(next_to) - strlen(fpath+ftwbuf->base);
-//        char temp = next_to[null_index];
-//        next_to[null_index] = 0;
-//        if (-1 == mkdir(next_to, sb->st_mode)) {
-//            if (EEXIST == errno) printf("%s exists\n", fpath + ftwbuf->base);
-//            else handle_error("mkdir failed");
-//        }
-//        next_to[null_index] = temp;
-
-        int to_fd = open(next_to, O_CREAT | O_WRONLY, sb->st_mode);
-        if (-1 == to_fd) handle_error("create target file");
-
-        int byte_read, byte_write;
-        while (0 < (byte_read = read(from_fd, content_buf, sizeof content_buf))) {
-            while ((byte_read = write(to_fd, content_buf, sizeof content_buf), byte_write > 0)) {
-                byte_write -= byte_read;
-            }
-            if (-1 == byte_write) handle_error("write failed");
+        size_t null_index = strlen(next_to) - strlen(fpath + ftwbuf->base);
+        char temp = next_to[null_index];
+        next_to[null_index] = 0;
+        if (-1 == mkdir_p(next_to)) {
+            if (EEXIST != errno) handle_error("mkdir failed");
         }
-        if (-1 == byte_read) handle_error("read failed");
-        if (-1 == utimensat(AT_FDCWD, next_to, times, 0)) handle_error("set file time");
+        next_to[null_index] = temp;
 
-        close(from_fd);
-        close(to_fd);
+        if (FTW_F == typeflag) {
+            int from_fd = open(fpath, O_RDONLY);
+            if (-1 == from_fd) handle_error("open source file");
+            int to_fd = open(next_to, O_CREAT | O_WRONLY, sb->st_mode);
+            if (-1 == to_fd) handle_error("create target file");
+
+            if (-1 == sendfile(to_fd, from_fd, 0, sb->st_size))
+                handle_error("send file");
+
+            close(from_fd);
+            close(to_fd);
+        } else {
+            realpath(fpath, rpath);
+            symlink(rpath, next_to);
+        }
+        if (-1 == utimensat(AT_FDCWD, next_to, times, AT_SYMLINK_NOFOLLOW))
+            handle_error("set file time");
     }
     return 0;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     if (argc < 3) puts("Not enough arguments");
 
-    strcpy(from, argv[1]);
-    strcpy(to, argv[2]);
+    realpath(argv[1], from);
+    realpath(argv[2], to);
+    strcat(from, "/");
+    strcat(to, "/");
 
-    if ('/' != from[strlen(from) - 1]) strcat(from, "/");
-    if ('/' != to[strlen(to) - 1]) strcat(to, "/");
     start_index = strlen(from);
 
-    int nftw_res = nftw(from, walk_action, 20, 0);//FTW_DEPTH);
+    int nftw_res = nftw(from, walk_action, 20, FTW_DEPTH | FTW_PHYS);
     if (-1 == nftw_res) {
         perror("nftw");
         exit(EXIT_FAILURE);
